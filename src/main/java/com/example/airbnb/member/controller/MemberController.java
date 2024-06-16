dfa18a5b8ee3f40388e5bb7414693552cf422d85
Jung Yunho <a01049048063@gmail.com>
2024-06-16 16:17:26 +0900

    feat: 컬럼 변경에 따른 에러 수정

--- src/main/java/com/example/airbnb/member/controller/MemberController.java
+++ src/main/java/com/example/airbnb/member/controller/MemberController.java
@@ -7,11 +7,9 @@ import com.example.airbnb.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -64,7 +62,6 @@ public class MemberController {
         modelAndView.setViewName("redirect:/house/houses");
         return modelAndView;
     }
-
 
 //    @GetMapping("/guest/mypage")
 //    public ModelAndView showMyPage() {
